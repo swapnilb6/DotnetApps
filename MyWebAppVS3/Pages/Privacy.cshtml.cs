7b9d45d2a3d7adb1d43651e0203da63755dd471d
Swapnil Bawankar <swapnilb6@gmail.com>
2026-09-21 01:47:13 -0700

    Initial Version , application created

---
+++ MyWebAppVS3/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebAppVS3.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
